f0d73c2d6672ca1960c17254f623e22a5c4a84bd
Caio Oliveira Lopes <122275651+caioolopes@users.noreply.github.com>
2026-04-25 14:32:06 +0000

    feat: implement JWT authentication with RS256 keys

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config'
 import { Env } from 'src/env'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
+import { AuthenticateController } from 'src/controllers/authenticate-controller'
 
 @Module({
   imports: [
@@ -10,12 +11,19 @@ import { PassportModule } from '@nestjs/passport'
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory(config: ConfigService<Env, true>) {
-        const secret = config.get('JWT_SECRET', { infer: true })
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
+
         return {
-          secret,
+          signOptions: {
+            algorithm: 'RS256',
+          },
+          privateKey: Buffer.from(privateKey, 'base64').toString('utf8'),
+          publicKey: Buffer.from(publicKey, 'base64').toString('utf8'),
         }
       },
     }),
   ],
+  controllers: [AuthenticateController],
 })
 export class AuthModule {}
