de63dd7d2cc733bc440b6dbaf55b5686eb31eecd
Caio Oliveira Lopes <122275651+caioolopes@users.noreply.github.com>
2026-05-05 00:43:30 +0000

    feat: add auth controller, create question and fetch recent questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { PrismaService } from './prisma/prisma.service'
+import { PrismaModule } from './prisma/prisma.module'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
   imports: [
@@ -12,8 +15,13 @@ import { AuthModule } from './auth/auth.module'
       isGlobal: true,
     }),
     AuthModule,
+    PrismaModule,
   ],
-  controllers: [CreateAccountController],
-  providers: [PrismaService],
+  controllers: [
+    CreateAccountController,
+    CreateQuestionController,
+    AuthenticateController,
+    FetchRecentQuestionsController,
+  ],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,29 +1,28 @@
 import { Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { Env } from 'src/env'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
-import { AuthenticateController } from 'src/controllers/authenticate-controller'
+import { Env } from 'src/env'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
+      global: true,
       useFactory(config: ConfigService<Env, true>) {
         const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
         const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
 
         return {
-          signOptions: {
-            algorithm: 'RS256',
-          },
-          privateKey: Buffer.from(privateKey, 'base64').toString('utf8'),
-          publicKey: Buffer.from(publicKey, 'base64').toString('utf8'),
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
         }
       },
     }),
   ],
-  controllers: [AuthenticateController],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
